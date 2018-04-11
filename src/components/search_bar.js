87c83ddb65eaee89bdc2fdc125cf2fd0321404ba
vanitanaidu <vanita112003@gmail.com>
2018-04-11 11:53:16 -0400

    initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  // handleOnChange = () => {
+  // debugger
+  //   // const value = this.event.target.value
+  // }
+
+
+  render() {
+    return <input />
+
+  }
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Route } from 'react-router';
+// import thunk from 'redux-thunk';
+
+// import SearchBar from './components/search_bar';
+
+// const API_KEY = 'AIzaSyAYrEUDDwKXMM1_AeulTVPcXkarKTmv3sY'
+const App = () => {
+   return <div> Hi </div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
